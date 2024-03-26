bba8bff4072e4195cfa90c33ba55afe0a2f05529
kristina <krisbubenka@icloud.com>
2024-03-26 20:00:39 +0300

    add handlebars plugins

--- main.js
+++ main.js
@@ -1,2 +1,7 @@
 import "lucide-static/font/lucide.css";
 import "./style.css";
+
+import template from "./counter.hbs";
+import { compile } from "handlebars";
+
+const root = document.getElementById("app");
